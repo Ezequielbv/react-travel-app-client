f141fd7673c5052641d4ed6d645929d039e22a72
Ezequiel Benitez <ezequielbv@gmail.com>
2022-12-12 17:12:21 +0100

    Profile pages with some country info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,40 @@
 import "./ProfilePage.css";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+// const API_COUNTRY = 'https://restcountries.com/v3.1/all';
+const country = 'germany';
+const API_COUNTRY = `https://restcountries.com/v3.1/name/${country}`;
 
 function ProfilePage() {
+
+  const [fetching, setFetching] = useState(true);
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    // console.log("useEffect - Initial render (Mounting)");
+    axios.get(API_COUNTRY)
+      .then((response) => {
+        setCountry(response.data[0]);
+        setFetching(false);
+      });
+  }, []);
+  country && console.log("country info", Object.entries(country.currencies)[0][1].name);
+
   return (
     <div>
       <h1>Profile page</h1>
+
+      <div className='countryInfo'>
+        <p>Capital: {country.capital}</p>
+        {/* <p>Currency: {Object.entries(country.currencies)[0][1].name}</p> */}
+        {/* <p>Language: {Object.entries(country.languages).map(el => {
+                return (
+                  <li>{el[1]}</li>
+                )
+              })}</p> */}
+
+      </div>
     </div>
   );
 }
