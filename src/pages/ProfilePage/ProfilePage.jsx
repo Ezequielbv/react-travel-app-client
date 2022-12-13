719d4c628dbc520c286f2afd222f1ab40964933a
Ezequiel Benitez <ezequielbv@gmail.com>
2022-12-13 18:49:40 +0100

    Profile page return countries and information from the countries - country info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -2,56 +2,39 @@ import "./ProfilePage.css";
 import React, { useState, useEffect, useContext } from "react";
 import { LocationFormContext } from "../../context/location-form.context";
 import { Dimmer, Loader }   from 'semantic-ui-react';
-
+import CountryInfo from "../../components/CountryInfo/CountryInfo";
 import axios          from 'axios';
 
-const COUNTRY_API_URL = 'https://restcountries.com/v3.1';
+const DB_BE_URL = 'http://localhost:5005';
 
 function ProfilePage() {
-  const { country }       = useContext(LocationFormContext);
-  const [countryData, setCountryData] = useState("");
-  const [fetching, setFetching]       = useState(true);
-  country && console.log(country);
-
-  useEffect(() => {
-    console.log("useEffect - Initial render (Mounting)");
-    axios.get(`${COUNTRY_API_URL}/name/${country}`)
-      .then((response) => {
-        console.log(response)
-        setCountryData(response.data);
-        setFetching(false);
-      });
-  }, []);
-  countryData && console.log(countryData)
-  countryData && console.log(countryData[0].name.nativeName);
+  const [countries, setCoutries] = useState();
   
-  //Languages
-  countryData && console.log(Object.entries(countryData[0].languages)[0])
-
-  return countryData ? 
-    (<>
+  useEffect(() => {
+    axios.get(`${DB_BE_URL}/api/profile`)
+    .then((response) =>{
+      setCoutries(response.data.location);
+    })
+  }, []);
+  countries && console.log("countries from BE", countries)
+  
+  return (
+    <>
       <div>
         <h1>Profile page</h1>
-        <div className='countryInfo'>
-          <p>Common name: { Object.entries(countryData[0].name.nativeName)[0][1].common }</p>
-          {/* <p>Native name: { countryData[0].name.nativeName } </p> */}
-          <p>Capital: { countryData[0].capital }</p>  
-          <p>Currency: {Object.entries(countryData[0].currencies)[0][1].name}</p>
-          <p>Languages: {Object.entries(countryData[0].languages).map(el => {
-                  return (
-                    <li>{el[1]}</li>
-                  )
-                })}</p> 
-          </div>
+
+        <div class="accordion" id="accordionExample">
+        {countries?.map((country) => {
+          return (
+            <>
+              <CountryInfo country={country}/>
+            </>
+          )
+        })}
+        </div>
       </div>
-    </>) :
-    (<>
-      <Dimmer active>
-                <Loader>
-                    Loading ...
-                </Loader>
-      </Dimmer>
-    </>);
+    </>
+    )
 }
 
 export default ProfilePage;
