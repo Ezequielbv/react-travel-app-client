bd77fbb0d5d8fa3a891330653d1bb8bfd5724337
Ezequiel Benitez <ezequielbv@gmail.com>
2022-12-15 23:39:18 +0100

    added general map to the homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import "./HomePage.css";
+import GenerateMap from "../../components/GenerateMap/GenerateMap";
 
 function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
+      <GenerateMap/>
     </div>
   );
 }
